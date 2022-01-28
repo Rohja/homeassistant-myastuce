cdeb122f22e76dd0b912c05d39a16ea693f4a1ce
Paul Rohja LESELLIER <rohja@rohja.com>
2022-01-28 10:15:51 +0100

    Try first config flow

--- custom_components/my_astuce_rouen/config_flow.py
+++ custom_components/my_astuce_rouen/config_flow.py
@@ -1,1 +1,23 @@
- 
+import voluptuous as vol
+
+from homeassistant import data_entry_flow, config_entries
+
+from . import DOMAIN
+
+@config_entries.HANDLERS.register(DOMAIN)
+class MyAstuceConfigFlow(data_entry_flow.FlowHandler):
+    """Handle a esphome config flow."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        # Specify items in the order they are to be displayed in the UI
+        data_schema = {
+            vol.Required("username"): str,
+            vol.Required("password"): str,
+        }
+
+        if self.show_advanced_options:
+            data_schema["allow_groups"] = bool
+
+        return self.async_show_form(step_id="init", data_schema=vol.Schema(data_schema))
