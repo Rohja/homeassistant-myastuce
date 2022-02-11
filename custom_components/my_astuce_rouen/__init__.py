c1da425f6286e744a369613ce43f5c4b7d4d48c7
Paul Rohja LESELLIER <rohja@rohja.com>
2022-02-11 14:33:45 +0100

    Try to implement CONFIG_SCHEMA

--- custom_components/my_astuce_rouen/__init__.py
+++ custom_components/my_astuce_rouen/__init__.py
@@ -5,17 +5,44 @@ configuration.yaml file.
 my_astuce_rouen:
 """
 from __future__ import annotations
+import logging
+
+import voluptuous as vol
 
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.typing import ConfigType
+
 
 # The domain of your component. Should be equal to the name of your component.
 DOMAIN = "my_astuce_rouen"
+_LOGGER = logging.getLogger(__name__)
+
+CONF_LINE_NAME = "line"
+CONF_STATION_NAME = "station"
+CONF_LINE_DIRECTION = "direction"
+
+LINE_STOP_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_LINE_NAME): cv.string,
+        vol.Required(CONF_LINE_NAME): cv.string,
+        vol.Required(CONF_LINE_NAME): vol.All(vol.Coerce(int), vol.Range(min=1, max=2))
+    }
+)
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        vol.Optional(DOMAIN, default={}): vol.All(
+            cv.ensure_list, [LINE_STOP_SCHEMA]
+        )
+    }
+)
 
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up a skeleton component."""
     # States are in the format DOMAIN.OBJECT_ID.
+    _LOGGER.info("Config:", config)
     hass.states.set('my_astuce_rouen.Hello_World', 'Works!')
 
     # Return boolean to indicate that initialization was successfully.
